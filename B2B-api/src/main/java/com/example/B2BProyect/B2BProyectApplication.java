ea8d2660c43e2a6634c9cd8d41e00e56d42ae558
Santiago Villanueva <S.V.S710x@gmail.com>
2026-05-29 20:20:51 -0400

    Stereum Api in early impl

--- B2B-api/src/main/java/com/example/B2BProyect/B2BProyectApplication.java
+++ B2B-api/src/main/java/com/example/B2BProyect/B2BProyectApplication.java
@@ -1,27 +1,55 @@
 package com.example.B2BProyect;
 
-import com.example.B2BProyect.service.integration.SistemaA;
+import com.example.B2BProyect.integracion.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.data.web.config.SpringDataWebSettings;
 
 @SpringBootApplication
 public class B2BProyectApplication implements CommandLineRunner {
 	@Autowired
-	private SistemaA sistemaA;
+    private SpringDataWebSettings springDataWebSettings;
+	@Autowired
+	private SistemaB2B sistemaB2B;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(B2BProyectApplication.class, args);
+
+		System.out.println("Hola mundo");
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		sistemaA.getAll(
-				"/api/v1/sucursales-empresa",
-				new ParameterizedTypeReference<>() {}
-		);
+		utilitario();
+//		B2BAuthRequest request = new B2BAuthRequest();
+//		request.setUsername("root");
+//		request.setPassword("Abc123**");
+//		B2BAuthResponse response = sistemaB2B.auth(request);
+//		System.out.println(sistemaB2B.listCategorias());
+//		System.out.println(sistemaB2B.list2());;
+//		System.out.println(response);
+		StereumApiRequest request1 = new StereumApiRequest();
+		request1.setCountry("BO");
+			request1.setAmount("6767");
+			request1.setCurrency("USDT");
+			request1.setNetwork("POLYGON");
+			request1.setChargeReason("Compra de prueba");
+			request1.setReservationValidityTime("10");
+		Customer customer = new Customer();
+			customer.setName("Ricardo");
+			customer.setLastName("Laredo");
+			customer.setDocumentNumber("76887344");
+		StereuemApiResponse response1 = sistemaB2B.callStereum(request1);
+		System.out.println(response1);
+	}
+
+	private void utilitario() {
+//		Prueba prueba = new Prueba();
+//		prueba.setNombre("Nueva prueba uwu");
+//		pruebaRepository.save(prueba);
 	}
 
 
