407e030eaa8903e08d30a7a01237d4e9b4e70b80
pyroneta <fernando.rodriguez.sanizo.1005@gmail.com>
2026-06-12 17:51:11 -0400

    arreglate error

--- data/src/main/java/com/example/B2BProyect/repository/LogRepository.java
+++ data/src/main/java/com/example/B2BProyect/repository/LogRepository.java
@@ -1,7 +1,22 @@
 package com.example.B2BProyect.repository;
 
-import com.example.B2BProyect.repository.entity.Log;
+import edu.upb.eventop.repository.entity.Log;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
+@Repository
 public interface LogRepository extends JpaRepository<Log, String> {
+
+    @Query("SELECT l FROM Log l WHERE l.createdDate BETWEEN :pInit AND :pEnd")
+    Page<Log> findAllByOrderByDateDesc(
+            @Param("pInit") LocalDateTime pInit,
+            @Param("pEnd") LocalDateTime pEnd,
+            Pageable pageable);
+
 }
