3c995dc085bbe26a84bfd9d2ec062a9dc02b96df
Ariana <ariana.mendivil@gmail.com>
2026-06-23 18:27:46 -0400

    Comiteado swagger

--- B2B-api/src/main/java/com/example/B2BProyect/controller/CatProveedorController.java
+++ B2B-api/src/main/java/com/example/B2BProyect/controller/CatProveedorController.java
@@ -8,7 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -16,7 +16,7 @@ import java.util.List;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 @RequestMapping("/api/v1/categorias-proveedor")
 public class CatProveedorController {
     private final CatProveedorService catProveedorService;
